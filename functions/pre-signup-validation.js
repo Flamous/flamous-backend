12105fbf87cfac71231d4acdb1a7003a6c96075c
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-05-11 16:42:24 +0200

    Try to add Cognito pre signup trigger to cognito

---
+++ functions/pre-signup-validation.js
@@ -0,0 +1,7 @@
+function deleteAlbumCover (event, context, callback) {
+    console.log('wohoooooooooo')
+    console.log(context)
+    console.log(event)
+  }
+
+  export { deleteAlbumCover as main }
